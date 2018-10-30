6832bfd4c5071712b3adde79d58faec96b1c0e68
daafith <davidbaak@gmail.com>
2018-10-30 10:23:38 +0100

    changed show one behavior

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View, TouchableOpacity, Animated, Switch} from 'react-native';
 import {styles} from "./styles";
-
 
 export default class App extends React.Component {
     constructor(props) {
@@ -10,13 +9,13 @@ export default class App extends React.Component {
             showFirst: false,
             showSecond: false,
             showThird: false,
-            displayOne: true
+            displayOneAtATime: true
         };
     }
 
     allowAll = (value) => {
         this.setState({
-            displayOne: value,
+            displayOneAtATime: value,
             showFirst: false,
             showSecond: false,
             showThird: false,
@@ -24,75 +23,114 @@ export default class App extends React.Component {
     }
 
     flip(index) {
-        if(index===0 && (!this.state.displayOne || (!this.state.showSecond && !this.state.showThird))){
+        if (this.state.displayOneAtATime) {
+            this.displayOne(index);
+        } else {
+            this.displayAny(index);
+        }
+    };
+
+    displayAny(index) {
+        if (index === 0) {
             this.setState({
                 showFirst: !this.state.showFirst
             })
-        } else if(index===1 && (!this.state.displayOne || (!this.state.showFirst && !this.state.showThird))){
+        } else if (index === 1) {
             this.setState({
                 showSecond: !this.state.showSecond
             })
-        } else if(index===2 && (!this.state.displayOne || (!this.state.showFirst && !this.state.showSecond))){
+        } else if (index === 2) {
             this.setState({
                 showThird: !this.state.showThird
             })
         }
-    };
+    }
+
+    displayOne(index) {
+        if (index === 0) {
+            this.setState({
+                showFirst: !this.state.showFirst,
+                showSecond: false,
+                showThird: false,
+            })
+        } else if (index === 1) {
+            this.setState({
+                showSecond: !this.state.showSecond,
+                showFirst: false,
+                showThird: false,
+            })
+        } else if (index === 2) {
+            this.setState({
+                showThird: !this.state.showThird,
+                showFirst: false,
+                showSecond: false,
+            })
+        }
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.nestedButtonView}>
-                <Switch
-                    onValueChange = {this.allowAll}
-                    value = {this.state.displayOne}/>
-                <Text style={styles.buttonText}>Een kaart tegelijk</Text>
+                    <Switch
+                        onValueChange={this.allowAll}
+                        value={this.state.displayOneAtATime}/>
+                    <Text style={styles.buttonText}>Eén kaart tegelijk</Text>
                 </View>
-                <View style={styles.spacing}>
-                </View>
+                <Spacer/>
                 <View>
 
                     <FadeIntoView>
                         {!this.state.showFirst &&
-                        <TouchableOpacity underlayColor={"transparent"} style={styles.kopCard} onPress={()=>this.flip(0)}>
+                        <TouchableOpacity underlayColor={"transparent"} style={styles.kopCard}
+                                          onPress={() => this.flip(0)}>
                             <Text style={styles.flipText}>Kop</Text>
                         </TouchableOpacity>}
                         {this.state.showFirst &&
-                            <TouchableOpacity underlayColor={"transparent"} style={styles.kopBack} onPress={()=>this.flip(0)}>
+                        <TouchableOpacity underlayColor={"transparent"} style={styles.kopBack}
+                                          onPress={() => this.flip(0)}>
                             <Text style={styles.flipText}>Weggelopen:</Text>
                         </TouchableOpacity>}
                     </FadeIntoView>
                 </View>
-                <View style={styles.spacing}>
-                </View>
+                <Spacer/>
                 <View>
                     <FadeIntoView>
                         {!this.state.showSecond &&
-                        <TouchableOpacity underlayColor={"transparent"} style={styles.aanbodCard} onPress={()=>this.flip(1)}>
+                        <TouchableOpacity underlayColor={"transparent"} style={styles.aanbodCard}
+                                          onPress={() => this.flip(1)}>
                             <Text style={styles.flipText}>Aanbod</Text>
                         </TouchableOpacity>}
                         {this.state.showSecond &&
-                        <TouchableOpacity underlayColor={"transparent"} style={styles.kopBack} onPress={()=>this.flip(1)}>
+                        <TouchableOpacity underlayColor={"transparent"} style={styles.kopBack}
+                                          onPress={() => this.flip(1)}>
                             <Text style={styles.flipText}>een gemeubileerde zitkamer</Text>
                         </TouchableOpacity>}
                     </FadeIntoView>
                 </View>
-                <View style={styles.spacing}>
-                </View>
+                <Spacer/>
                 <View>
                     <FadeIntoView>
                         {!this.state.showThird &&
-                        <TouchableOpacity underlayColor={"transparent"} style={styles.beschrijvingCard} onPress={()=>this.flip(2)}>
+                        <TouchableOpacity underlayColor={"transparent"} style={styles.beschrijvingCard}
+                                          onPress={() => this.flip(2)}>
                             <Text style={styles.flipText}>Beschrijving</Text>
                         </TouchableOpacity>}
                         {this.state.showThird &&
-                        <TouchableOpacity underlayColor={"transparent"} style={styles.kopBack} onPress={()=>this.flip(2)}>
+                        <TouchableOpacity underlayColor={"transparent"} style={styles.kopBack}
+                                          onPress={() => this.flip(2)}>
                             <Text style={styles.flipText}>in rolletjes van 20 stuks</Text>
                         </TouchableOpacity>}
                     </FadeIntoView>
                 </View>
             </View>
         );
+    }
+}
+
+class Spacer extends Component {
+    render() {
+        return (<View style={styles.spacing}></View>);
     }
 }
 
