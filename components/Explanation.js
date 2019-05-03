f07ee99484be949a565d9d976b8c881d55da0cc8
daafith <davidbaak@gmail.com>
2019-05-03 09:48:19 +0200

    improved explanation

--- components/Explanation.js
+++ components/Explanation.js
@@ -10,11 +10,11 @@ export default class Explanation extends Component {
                         <Text style={{fontWeight: "bold"}}>Spelregels</Text>{"\n"}
                         Het advertentiespel bevat drie soorten kaarten: de kop, het aanbod en de
                         beschrijving.{"\n"}{"\n"}
-                        Om de beurt draaien de spelers een kaart om en lezen hardop wat er op staat.
-                        Eerst een kopkaart, dan het aanbod en tenslotte een beschrijving.
+                        Om de beurt draaien de spelers een kaart om en lezen hardop voor wat er op staat.
+                        Eerst een kopkaart, dan het aanbod en ten slotte een beschrijving.
                         Het doel van het spel is gezelligheid en plezier.{"\n"}{"\n"}
                         <Text style={{fontWeight: "bold"}}>Opties</Text>{"\n"}
-                        Deze app geeft (per speler) de mogelijkheid één of meerdere kaarten tegelijk per beurt te trekken.
+                        Deze app geeft de mogelijkheid (per speler) één of meerdere kaarten tegelijk per beurt te trekken.
                         Wil je een andere tekst? Druk dan opnieuw op de betreffende kaart.{"\n"}
                     </Text>
             </View>
